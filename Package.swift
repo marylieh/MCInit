7d6aeafe7a8d6d08c24541528e769a3c26bb819a
marylieh <m.aschmann@icloud.com>
2025-01-03 23:40:34 +0100

    Add example swift test

--- Package.swift
+++ Package.swift
@@ -5,10 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "MCInit",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "MCInit"),
+            name: "MCInit",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]),
+        .testTarget(name: "MCInitTests",
+                    dependencies: ["MCInit"])
     ]
 )
---
+++ Tests/MCInitTests/MCInitTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import MCInit
+
+final class MCInitTests: XCTestCase {
+    func testExample() {
+        XCTAssertEqual(1 + 1, 2, "Example geht")
+    }
+}
